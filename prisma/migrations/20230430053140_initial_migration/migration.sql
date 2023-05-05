aaa0225b62e7d279721875e78117ce843db302e4
ADamian17 <adonisdesuvikdo@gmail.com>
2023-05-04 21:16:24 -0700

    updated prisma

--- prisma/migrations/20230430053140_initial_migration/migration.sql
+++ prisma/migrations/20230430053140_initial_migration/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE "Product" (
     "id" SERIAL NOT NULL,
     "name" VARCHAR(255) NOT NULL,
     "image" TEXT NOT NULL,
-    "price" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
